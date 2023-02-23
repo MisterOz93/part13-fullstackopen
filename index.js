f4e153e37d2a3f780bdeb5624ad2568d026b4cf4
Michael Mroz <mroz15@hotmail.com>
2023-02-23 14:38:31 -0400

    complete 13.4 - add GET/POST/DELETE routes for blogs, add blogs.rest file to test API calls

--- index.js
+++ index.js
@@ -13,8 +13,57 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   },
 });
 
-app.get('/', (req, res) => {
-    res.send('PostgreSQL DB @ elephantsql.')
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body)
+    return res.json(newBlog) 
+  } catch (e) {
+    return res.status(400).json({e})
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog){
+    await blog.destroy()
+  }
+  return res.status(201)
 })
 
 const PORT = process.env.PORT || 3001
