e8bfa102a680b34aa6db1af5000c00dee8f962c1
Michael Mroz <mroz15@hotmail.com>
2023-03-05 17:18:03 -0400

    complete 13.22 - add /api/readinglists/:id PUT route to change readlingList.read value using token authentication

--- controllers/readingList.js
+++ controllers/readingList.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
+const { tokenExtractor } = require('../util/middleware')
 
-const { ReadingList } = require('../models')
+const { ReadingList, User } = require('../models')
 
 router.post('/', async (req, res, next) => {
     const { blogId, userId } = req.body
@@ -17,6 +18,25 @@ router.get('/', async (req, res) => {
     const blogsInList = await ReadingList.findAll()
 
     return res.json(blogsInList)
+})
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+  try {
+    const user = await User.findOne({where: {username: req.decodedToken.username}})
+    const readingList = await ReadingList.findByPk(req.params.id)
+   
+    if (user.id !== readingList.userId){
+      return res.status(400).json({error: "User can only mark his/her own readinglist as read"})
+    }
+    
+    readingList.read = req.body.read
+    await readingList.save()
+    res.json(readingList)
+
+  } catch (e) {
+    next(e)
+    return res.status(400)
+  }
 })
 
 module.exports = router
